51bdf860887aba9e8fa7e7da98a2ba18837e7961
Georgi Karadzhov <gmk34@cam.ac.uk>
2019-11-06 10:16:42 +0000

    minor changes in env naming

--- app.py
+++ app.py
@@ -64,23 +64,27 @@ def handle_my_custom_event(json, methods=('GET', 'POST')):
     
 @app.route('/test_s3')
 def sign_s3():
-  S3_BUCKET = os.environ.get('S3_BUCKET')
+  S3_BUCKET = os.environ.get('S3_BUCKET_NAME')
+
+  file_name = "data/rooms.tsv"
+  file_type = "tsv"
+
   s3 = boto3.client('s3')
 
   presigned_post = s3.generate_presigned_post(
-    Bucket=S3_BUCKET,
-    Key="text",
-    Fields = {"acl": "public-read", "Content-Type": "text"},
+    Bucket = S3_BUCKET,
+    Key = file_name,
+    Fields = {"acl": "public-read", "Content-Type": file_type},
     Conditions = [
       {"acl": "public-read"},
-      {"Content-Type": "text"}
+      {"Content-Type": file_type}
     ],
     ExpiresIn=3600
   )
 
   return json.dumps({
     'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, "text")
+    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
   })
 
 
