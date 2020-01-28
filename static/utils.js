9ab6b13872a14907f4c58d3c4a45e40b53873e63
Georgi Karadzhov <gmk34@cam.ac.uk>
2020-01-28 17:31:02 +0000

    javascript refactoring and added inactivity watcher

---
+++ static/utils.js
@@ -0,0 +1,115 @@
+const JOIN_ROOM = 'JOIN_ROOM';
+const CHAT_MESSAGE = 'CHAT_MESSAGE';
+const LEAVE_ROOM = 'LEAVE_ROOM';
+const FINISHED_ONBOARDING = 'FINISHED_ONBOARDING';
+const WASON_INITIAL = 'WASON_INITIAL';
+const WASON_GAME = 'WASON_GAME';
+const WASON_AGREE = 'WASON_SUBMIT';
+const WASON_FINISHED = 'WASON_FINISHED';
+
+const USR_ONBOARDING = 'USR_ONBOARDING';
+const USR_PLAYING = 'USR_PLAYING';
+const USR_MODERATING = 'USR_MODERATING';
+
+
+ROUTING_TIMER_STARTED = 'ROUTING_TIMER_STARTED'
+ROUTING_TIMER_ELAPSED = 'ROUTING_TIMER_ELAPSED'
+
+READY_TO_START = "READY_TO_START"
+ROOM_PLAYING = "ROOM_PLAYING"
+
+function split(val){
+  return val.split( / \s*/ );
+}
+
+function extractLast(term) {
+  return split(term).pop();
+}
+
+function boldMentions(str){
+    var regex = new RegExp("@\\w*", 'gi');
+    str = str.replace(regex, function(str) {return '<b>'+str+'</b>'});
+    return str;
+}
+
+function start_timer(count_down, visualise, callback) {
+    var now = new Date().getTime();
+    // Find the distance between now and the count down date
+    var distance = count_down - now;
+
+    // Time calculations for days, hours, minutes and seconds
+
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (visualise == true){
+        $('#timer').text(minutes + " minutes " + seconds + " seconds");
+    }
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        console.log('distance');
+        callback();
+    }
+}
+
+
+function get_card_selection(){
+    var selection = [];
+    $("input:checkbox").each(function(){
+        selection.push({'value': $(this).val(), 'checked': $(this).is(":checked")});
+    });
+
+    return selection;
+}
+
+
+function wakeup(){
+    alert("You've been inactive for a while, the game is starting soon.");
+}
+
+
+
+
+function activityWatcher(kick_callback){
+
+    var secondsSinceLastActivity = 0;
+
+    var kick_inactivity = (60 * 4);
+    var notification_inactivity = (30 * 5)
+    //Setup the setInterval method to run
+    //every second. 1000 milliseconds = 1 second.
+    setInterval(function(){
+        secondsSinceLastActivity++;
+        if(secondsSinceLastActivity > notification_inactivity){
+            console.log('User has been inactive for more than ' + kick_inactivity + ' seconds');
+            //Redirect them to your logout.php page.
+            wakeup();
+        }
+
+        if(secondsSinceLastActivity > kick_inactivity){
+            console.log('User has been inactive for more than ' + kick_inactivity + ' seconds');
+            //Redirect them to your logout.php page.
+            kick_callback();
+        }
+    }, 1000);
+
+    //The function that will be called whenever a user is active
+    function activity(){
+        //reset the secondsSinceLastActivity variable
+        //back to 0
+        secondsSinceLastActivity = 0;
+    }
+
+    //An array of DOM events that should be interpreted as
+    //user activity.
+    var activityEvents = [
+        'mousedown', 'mousemove', 'keydown',
+        'scroll', 'touchstart'
+    ];
+
+    //add these events to the document.
+    //register the activity function as the listener parameter.
+    activityEvents.forEach(function(eventName) {
+        document.addEventListener(eventName, activity, true);
+    });
+}
