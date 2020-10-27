9c139ed45fbfdbfe909f4d02adce96b4b47b2232
Georgi Karadzhov <gogokaradjov@gmail.com>
2020-10-27 14:25:21 +0000

    Target blank for the outro url

--- app.py
+++ app.py
@@ -432,7 +432,7 @@ def validate_finish_game(all_messages, room_id):
         all_messages.append(m)
 
         outro_content = "Thank you for your participation! Please fill " \
-                        "<a href='https://sheffieldpsychology.eu.qualtrics.com/jfe/form/SV_0ibJOwF7AiNXFfT?roomid={0}'>this questionnaire</a>".format(
+                        "<a target='_blank' href='https://sheffieldpsychology.eu.qualtrics.com/jfe/form/SV_0ibJOwF7AiNXFfT?roomid={0}'>this questionnaire</a>".format(
             room_id)
         outro_message = Message(origin_id=SYSTEM_ID, origin_name=SYSTEM_USER, message_type=CHAT_MESSAGE,
                                 room_id=room_id, content={'annotation': 'no_annotation', 'message': outro_content})
