a5e4c408562017a8349e93d7e87338108f67c11f
Georgi Karadzhov <gogokaradjov@gmail.com>
2022-11-17 14:27:29 +0000

    re-enable db

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ nlp = spacy.load('en_core_web_sm')
 
 login_manager.init_app(app)
 
-#PG = PostgreConnection('localadasdda_cred.json')
+PG = PostgreConnection('localadasdda_cred.json')
 MTURK_MANAGEMENT = MTurkManagement('local_creddadasasd.json')
 admin_pass = os.environ.get('ADMIN')
 salt = os.environ.get('SALT')
